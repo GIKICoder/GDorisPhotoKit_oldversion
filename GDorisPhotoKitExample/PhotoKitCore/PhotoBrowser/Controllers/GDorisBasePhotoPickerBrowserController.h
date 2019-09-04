420ded8774c160b1286ce7f579c317643c430fec
GIKI <gongke2014@xiaochuankeji.cn>
2019-09-04 21:57:14 +0800

    commit: update code add

---
+++ GDorisPhotoKitExample/PhotoKitCore/PhotoBrowser/Controllers/GDorisBasePhotoPickerBrowserController.h
@@ -0,0 +1,17 @@
+//
+//  GDorisBasePhotoPickerBrowserController.h
+//  GDorisPhotoKitExample
+//
+//  Created by GIKI on 2019/9/4.
+//  Copyright © 2019 GIKI. All rights reserved.
+//
+
+#import "GDorisBasePhotoBrowserController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GDorisBasePhotoPickerBrowserController : GDorisBasePhotoBrowserController
+
+@end
+
+NS_ASSUME_NONNULL_END
