0d81bcab587a661a2f67c13e35dc0f282fd0d318
GIKI <gongke2014@xiaochuankeji.cn>
2019-09-08 15:48:27 +0800

    commit: update large code

---
+++ GDorisPhotoKitExample/GDorisLargeImage/LargeImage/GDorisTiledImageBuilder.h
@@ -0,0 +1,17 @@
+//
+//  GDorisTiledImageBuilder.h
+//  GDorisPhotoKitExample
+//
+//  Created by GIKI on 2019/9/7.
+//  Copyright © 2019 GIKI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GDorisTiledImageBuilder : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GDorisPhotoKitExample/GDorisLargeImage/LargeImage/GDorisTiledImageBuilder.m
@@ -0,0 +1,13 @@
+//
+//  GDorisTiledImageBuilder.m
+//  GDorisPhotoKitExample
+//
+//  Created by GIKI on 2019/9/7.
+//  Copyright © 2019 GIKI. All rights reserved.
+//
+
+#import "GDorisTiledImageBuilder.h"
+
+@implementation GDorisTiledImageBuilder
+
+@end
